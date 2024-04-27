d7ee638d8a42fef880e0774a8a41c913aaa9af4e
Samuli <samyle@proton.me>
2024-04-27 22:21:42 +0300

    format scripts

--- Documents/scripts/sync-sub.sh
+++ Documents/scripts/sync-sub.sh
@@ -8,42 +8,40 @@ else
   DIR="$1"
 fi
 # Get all the MKV files in this dir and its subdirs
-find "$DIR" -type f -name '*.mkv' | while read filename
-do
+find "$DIR" -type f -name '*.mkv' | while read filename; do
   # Find out which tracks contain the subtitles
-  mkvmerge "$filename" | grep 'subtitles' | while read subline
-  do
+  mkvmerge "$filename" | grep 'subtitles' | while read subline; do
     echo "extract $filename"
     format="srt"
-    case "$subline" in 
-      *SubStationAlpha*)
-        format="ass"
-        ;;
+    case "$subline" in
+    *SubStationAlpha*)
+      format="ass"
+      ;;
     esac
     # Grep the number of the subtitle track
-    tracknumber=`echo $subline | egrep -o "[0-9]{1,2}" | head -1`
+    tracknumber=$(echo $subline | egrep -o "[0-9]{1,2}" | head -1)
 
     # Get base name for subtitle
     subtitlename=${filename%.*}
 
     # Extract the track to a .tmp file
-    `mkvextract tracks "$filename" $tracknumber:"$subtitlename.tmp.$format" > /dev/null 2>&1`
-    `chmod g+rw "$subtitlename.tmp.$format"`
+    $(mkvextract tracks "$filename" $tracknumber:"$subtitlename.tmp.$format" >/dev/null 2>&1)
+    $(chmod g+rw "$subtitlename.tmp.$format")
 
     # Convert .ass to .srt
-    case "$format" in 
-      *ass*)
-        echo "convert ass to srt"
-       `ffmpeg "$subtitlename.tmp.$format" -c:s srt "$subtitlename.tmp.srt"`
-        `rm "$subtitlename.tmp.$format"`
-        ;;
+    case "$format" in
+    *ass*)
+      echo "convert ass to srt"
+      $(ffmpeg "$subtitlename.tmp.$format" -c:s srt "$subtitlename.tmp.srt")
+      $(rm "$subtitlename.tmp.$format")
+      ;;
     esac
 
     # Resync subtitles using ffsubsync
     echo "sync $subtitlename.tmp.srt"
-    `ffsubsync "$subtitlename.tmp.srt" "$subtitlename.srt" -o "output.srt"`
-    `rm "$subtitlename.tmp.srt"`
-    `mv "output.srt" "$subtitlename.srt"` 
-    `chmod g+rw "$subtitlename.srt"`
+    $(ffsubsync "$subtitlename.tmp.srt" "$subtitlename.srt" -o "output.srt")
+    $(rm "$subtitlename.tmp.srt")
+    $(mv "output.srt" "$subtitlename.srt")
+    $(chmod g+rw "$subtitlename.srt")
   done
 done
